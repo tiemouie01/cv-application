2c00c7283d0665be9d88c7c284d18ec37ee4320a
Timothy Miamba <timal0361@gmail.com>
2024-05-15 06:28:20 +0200

    chore: Add Resume component to display general information

---
+++ src/components/Resume.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+const Resume = ({ generalInfo }) => {    
+  return (
+    <section>
+      <article>
+        <h1>{generalInfo.name}</h1>
+        <p>{generalInfo.email}</p>
+        <p>{generalInfo.phone}</p>
+      </article>
+    </section>
+  );
+};
+
+export default Resume;
