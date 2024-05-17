44a689d18062cd5d3418c3174dbfe7d0acfde88e
Timothy Miamba <timal0361@gmail.com>
2024-05-17 15:23:42 +0200

    refactor: Improve form submission handling in General component

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -7,53 +7,56 @@ const General = ({ generalInfo, handler }) => {
   const [currentPhone, setCurrentPhone] = useState(generalInfo.phone);
 
   return (
-    <form action="#">
-      <p>
-        <label htmlFor="name">Fullname: </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={currentName}
-          onChange={(e) => {
-            setCurrentName(e.target.value);
+    <section>
+      <h1>Personal Information</h1>
+      <form action="#">
+        <p>
+          <label htmlFor="name">Fullname: </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={currentName}
+            onChange={(e) => {
+              setCurrentName(e.target.value);
+            }}
+          />
+        </p>
+        <p>
+          <label htmlFor="mail">Email: </label>
+          <input
+            type="email"
+            name="mail"
+            id="mail"
+            value={currentEmail}
+            onChange={(e) => {
+              setCurrentEmail(e.target.value);
+            }}
+          />
+        </p>
+        <p>
+          <label htmlFor="phone-number">Phone Number: </label>
+          <input
+            type="text"
+            name="phone"
+            id="phone-number"
+            value={currentPhone}
+            onChange={(e) => {
+              setCurrentPhone(e.target.value);
+            }}
+          />
+        </p>
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            handler(currentName, currentEmail, currentPhone);
           }}
-        />
-      </p>
-      <p>
-        <label htmlFor="mail">Email: </label>
-        <input
-          type="email"
-          name="mail"
-          id="mail"
-          value={currentEmail}
-          onChange={(e) => {
-            setCurrentEmail(e.target.value);
-          }}
-        />
-      </p>
-      <p>
-        <label htmlFor="phone-number">Phone Number: </label>
-        <input
-          type="text"
-          name="phone"
-          id="phone-number"
-          value={currentPhone}
-          onChange={(e) => {
-            setCurrentPhone(e.target.value);
-          }}
-        />
-      </p>
-      <button
-        type="submit"
-        onClick={(e) => {
-          e.preventDefault();
-          handler(currentName, currentEmail, currentPhone);
-        }}
-      >
-        Submit
-      </button>
-    </form>
+        >
+          Submit
+        </button>
+      </form>
+    </section>
   );
 };
 
