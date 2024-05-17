088b857f35899fc5d79d32934f2ad1bd82bebe7c
Timothy Miamba <timal0361@gmail.com>
2024-05-17 16:19:13 +0200

    chore: Update Education component to toggle between edit and submit mode

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -5,57 +5,69 @@ const Education = ({ educationInfo, handleEducationInfo }) => {
   const [schoolName, setSchoolName] = useState(educationInfo.school);
   const [programme, setProgramme] = useState(educationInfo.programme);
   const [completionYear, setCompletionYear] = useState(educationInfo.year);
+  const [filled, setFilled] = useState(false);
 
   return (
     <section>
       <h1>Educational Information</h1>
-      <form action="#">
-        <p>
-          <label htmlFor="school">School Name: </label>
-          <input
-            type="text"
-            id="school"
-            name="school"
-            value={schoolName}
-            onChange={(e) => {
-              setSchoolName(e.target.value);
-            }}
-          />
-        </p>
-        <p>
-          <label htmlFor="prg">Programme of Study: </label>
-          <input
-            type="text"
-            name="programme"
-            id="prg"
-            value={programme}
-            onChange={(e) => {
-              setProgramme(e.target.value);
-            }}
-          />
-        </p>
-        <p>
-          <label htmlFor="year">Year of Completion</label>
-          <input
-            type="text"
-            name="year"
-            id="year"
-            value={completionYear}
-            onChange={(e) => {
-              setCompletionYear(e.target.value);
-            }}
-          />
-        </p>
+      {filled ? (
         <button
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            handleEducationInfo(schoolName, programme, completionYear);
+          onClick={() => {
+            setFilled(false);
           }}
         >
-          Submit
+          Edit
         </button>
-      </form>
+      ) : (
+        <form action="#">
+          <p>
+            <label htmlFor="school">School Name: </label>
+            <input
+              type="text"
+              id="school"
+              name="school"
+              value={schoolName}
+              onChange={(e) => {
+                setSchoolName(e.target.value);
+              }}
+            />
+          </p>
+          <p>
+            <label htmlFor="prg">Programme of Study: </label>
+            <input
+              type="text"
+              name="programme"
+              id="prg"
+              value={programme}
+              onChange={(e) => {
+                setProgramme(e.target.value);
+              }}
+            />
+          </p>
+          <p>
+            <label htmlFor="year">Year of Completion</label>
+            <input
+              type="text"
+              name="year"
+              id="year"
+              value={completionYear}
+              onChange={(e) => {
+                setCompletionYear(e.target.value);
+              }}
+            />
+          </p>
+          <button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              handleEducationInfo(schoolName, programme, completionYear);
+              setFilled(true);
+            }}
+          >
+            Submit
+          </button>
+        </form>
+      )}
     </section>
   );
 };
