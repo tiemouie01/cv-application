ea69f9518a26e7fce6ef463f0dacf93761b7a579
Timothy Miamba <timal0361@gmail.com>
2024-05-17 15:52:00 +0200

    chore: Add Education Experience to CV

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@ import "./App.css";
 import React, { useState } from "react";
 import General from "./components/General";
 import Resume from "./components/Resume";
+import Education from "./components/Education";
 
 function App() {
   const [generalInfo, setGeneralInfo] = useState({});
+  const [educationInfo, setEducationInfo] = useState({});
 
   const handleGeneralInfo = (name, email, phone) => {
     setGeneralInfo({ name, email, phone });
   };
 
+  const handleEducationInfo = (school, programme, year) => {
+    setEducationInfo({ school, programme, year });
+  };
+
   return (
     <main>
       <General generalInfo={generalInfo} handler={handleGeneralInfo} />
-      <Resume generalInfo={generalInfo} />
+      <Education
+        educationInfo={educationInfo}
+        handleEducationInfo={handleEducationInfo}
+      />
+      <Resume generalInfo={generalInfo} educationInfo={educationInfo} />
     </main>
   );
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable react/prop-types */
-const Resume = ({ generalInfo }) => {
+const Resume = ({ generalInfo, educationInfo }) => {
   return (
     <section>
       <article>
         <h1>{generalInfo.name}</h1>
         <p>{generalInfo.email}</p>
         <p>{generalInfo.phone}</p>
+      </article>
+      <article>
+        <h2>Educational Experience</h2>
+        <p>
+          School: {educationInfo.school}<br />
+          Programme of Study: {educationInfo.programme} <br />
+          Year of Completion: {educationInfo.year}
+        </p>
       </article>
     </section>
   );
